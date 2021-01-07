6c704c8def625659ea67f2ea9a00ba9aab3f443e
Blaise Sebagabo <bsebagabo1@studentmail.nait.ca>
2021-01-07 16:52:43 -0700

    prop

--- pages/header.js
+++ pages/header.js
@@ -1,3 +1,3 @@
 export default function Header({ someProp }) {
-  return <h1>Navigation {someProp.last_updated}</h1>;
+  return <h1>Navigation {someProp ? someProp.last_updated : 'prop data not available'}</h1>;
 }
